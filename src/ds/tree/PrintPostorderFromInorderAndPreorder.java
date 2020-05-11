31c3e48d962c83830b603f958fabb4998aae79df
nitinat007 <nitinkumar@Nitins-MacBook-Pro.local>
2020-05-11 23:58:52 +0530

    minor changes in info

--- src/ds/tree/PrintPostorderFromInorderAndPreorder.java
+++ src/ds/tree/PrintPostorderFromInorderAndPreorder.java
@@ -3,7 +3,8 @@ package ds.tree;
 /**
  * Author: nitinkumar
  * Created Date: 11/05/20
- * Info: Print Postorder traversal of tree from given inorder and preorder traversal
+ * Info: Print Postorder traversal of tree from given inorder and preorder traversal. Approach 1: make tree and do traversal. Approach2: Without making tree
+ * Order: O(n2)
  **/
 
 public class PrintPostorderFromInorderAndPreorder {
---
+++ src/ds/tree/bst/PrintPostorderFromPreorder.java
@@ -0,0 +1,10 @@
+package ds.tree.bst;
+
+/**
+ * Author: nitinkumar
+ * Created Date: 11/05/20
+ * Info: Print Postorder traversal of BST from given Preorder Traversal
+ **/
+
+public class PrintPostorderFromPreorder {
+}
