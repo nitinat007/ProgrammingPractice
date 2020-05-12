e20b741b97e6964ee4ecbf0fdea792e2a9878412
nitinat007 <nitinkumar@Nitins-MacBook-Pro.local>
2020-05-12 12:36:22 +0530

    Assigning level to problems

--- src/ds/tree/bst/PrintPostorderFromPreorder.java
+++ src/ds/tree/bst/PrintPostorderFromPreorder.java
@@ -4,7 +4,7 @@ package ds.tree.bst;
  * Author: nitinkumar
  * Created Date: 11/05/20
  * Info: Print Postorder traversal of BST from given Preorder Traversal
- * Important
+ * Level: 4
  **/
 
 public class PrintPostorderFromPreorder {
