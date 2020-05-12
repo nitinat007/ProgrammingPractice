d36ce32325942b06067d07875b73fe8b500b468b
nitinat007 <nitinkumar@Nitins-MacBook-Pro.local>
2020-05-12 11:43:44 +0530

    Changed md file name

--- src/ds/tree/bst/PrintPostorderFromPreorder.java
+++ src/ds/tree/bst/PrintPostorderFromPreorder.java
@@ -22,28 +22,28 @@ public class PrintPostorderFromPreorder {
     private static void printPostorder(int[] pre, int leftIndex, int rightIndex) {
         if (preIndex < n) {
             int val = pre[preIndex];
-            System.out.println("***\n val="+val);
+            System.out.println("***\n val=" + val);
             int indexValueGTValue = findIndexWhoseValueIsGreaterThanGivenValue(pre, val, preIndex, rightIndex);
             preIndex++;
-            System.out.println("indexValueGTValue: "+indexValueGTValue);
+            System.out.println("indexValueGTValue: " + indexValueGTValue);
 
-            if(indexValueGTValue>=0) {
-                System.out.println("method called for "+leftIndex+" to "+indexValueGTValue);
+            if (indexValueGTValue >= 0) {
+                System.out.println("method called for " + leftIndex + " to " + indexValueGTValue);
                 printPostorder(pre, leftIndex, indexValueGTValue);
                 System.out.println("method called for " + indexValueGTValue + " to " + rightIndex);
                 printPostorder(pre, indexValueGTValue, rightIndex);
             }
             System.out.println("Printing Value");
             System.out.println(val + "<-- ");
-           // preIndex++;
+            // preIndex++;
         }
     }
 
     private static int findIndexWhoseValueIsGreaterThanGivenValue(int[] pre, int val, int leftBoundary, int rightBoundary) {
-        System.out.println("Finding index for which value is greater than "+val+" . Range "+leftBoundary+" to "+rightBoundary);
+        System.out.println("Finding index for which value is greater than " + val + " . Range " + leftBoundary + " to " + rightBoundary);
         for (int i = leftBoundary; i < rightBoundary; i++) {
             if (pre[i] > val) {
-                return i ;
+                return i;
             }
         }
         return -1;
