0770e784be5f53a57e2692507a80d16941e788a5
nitinat007 <nitinkumar@Nitins-MacBook-Pro.local>
2020-05-12 12:32:19 +0530

    Correcting logic for PrintPostorderFromPreorder.java

--- src/ds/tree/bst/PrintPostorderFromPreorder.java
+++ src/ds/tree/bst/PrintPostorderFromPreorder.java
@@ -4,6 +4,7 @@ package ds.tree.bst;
  * Author: nitinkumar
  * Created Date: 11/05/20
  * Info: Print Postorder traversal of BST from given Preorder Traversal
+ * Important
  **/
 
 public class PrintPostorderFromPreorder {
@@ -12,7 +13,7 @@ public class PrintPostorderFromPreorder {
     static int n;
 
     public static void main(String[] args) {
-        int pre[] = {40, 30, 35, 80, 100};
+        int pre[] = {40, 30, 35, 80, 100}; // expected 35 30 100 80 40
         n = pre.length;
         System.out.print("Postorder Traversal: ");
         printPostorder(pre, 0, n);
@@ -20,27 +21,27 @@ public class PrintPostorderFromPreorder {
     }
 
     private static void printPostorder(int[] pre, int leftIndex, int rightIndex) {
+
+        if (leftIndex >= rightIndex) {
+            return;
+        }
         if (preIndex < n) {
-            int val = pre[preIndex];
-            System.out.println("***\n val=" + val);
+            int val = pre[preIndex++];
+
             int indexValueGTValue = findIndexWhoseValueIsGreaterThanGivenValue(pre, val, preIndex, rightIndex);
-            preIndex++;
-            System.out.println("indexValueGTValue: " + indexValueGTValue);
 
             if (indexValueGTValue >= 0) {
-                System.out.println("method called for " + leftIndex + " to " + indexValueGTValue);
-                printPostorder(pre, leftIndex, indexValueGTValue);
-                System.out.println("method called for " + indexValueGTValue + " to " + rightIndex);
+                printPostorder(pre, preIndex, indexValueGTValue);
                 printPostorder(pre, indexValueGTValue, rightIndex);
+                System.out.print(val + " ");
+            } else {
+                System.out.print(val + " ");
             }
-            System.out.println("Printing Value");
-            System.out.println(val + "<-- ");
-            // preIndex++;
+
         }
     }
 
     private static int findIndexWhoseValueIsGreaterThanGivenValue(int[] pre, int val, int leftBoundary, int rightBoundary) {
-        System.out.println("Finding index for which value is greater than " + val + " . Range " + leftBoundary + " to " + rightBoundary);
         for (int i = leftBoundary; i < rightBoundary; i++) {
             if (pre[i] > val) {
                 return i;
@@ -52,4 +53,7 @@ public class PrintPostorderFromPreorder {
 
 }
 
-//35 30 100 80 40
+/*
+O/P:
+Postorder Traversal: 35 30 100 80 40
+ */
