486be0d98c39fb928a8f64dc75680fb28d33e5c6
nitinat007 <nitinkumar@Nitins-MacBook-Pro.local>
2020-06-09 05:54:48 +0530

    Thread pool using ExecutorCompletionService

--- src/javaexamples/multithreading/ThreadPoolExamples.java
+++ src/javaexamples/multithreading/ThreadPoolExamples.java
@@ -8,11 +8,12 @@ public class ThreadPoolExamples {
     public static void main(String[] args) {
 
         FactorialCalculationWork task1 = new FactorialCalculationWork(5);
-        FactorialCalculationWork task2 = new FactorialCalculationWork(10);
-        FactorialCalculationWork task3 = new FactorialCalculationWork(12);
+        FactorialCalculationWork task2 = new FactorialCalculationWork(6);
+        FactorialCalculationWork task3 = new FactorialCalculationWork(7);
         FactorialCalculationWork task4 = new FactorialCalculationWork(14);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
+        //Using ExecutorService
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
 
         List<Future> futures = new ArrayList<>();
         futures.add(executorService.submit(task1));
@@ -22,6 +23,29 @@ public class ThreadPoolExamples {
 
         printOutput(futures);
         executorService.shutdown();
+        System.out.println();
+
+        //Using ExecutorCompletionService
+        ExecutorService executorService1 = Executors.newFixedThreadPool(2);
+        CompletionService executorCompletionService = new ExecutorCompletionService<>(executorService1);
+        List<Future> futures1 = new ArrayList<>();
+        futures1.add(executorCompletionService.submit(task1));
+        futures1.add(executorCompletionService.submit(task2));
+        futures1.add(executorCompletionService.submit(task3));
+        futures1.add(executorCompletionService.submit(task4));
+
+        for (int i = 0; i < 4; i++) {
+            try {
+                System.out.println(executorCompletionService.take().get());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //printOutput(futures1); // Will print results in the order of submitted task
+        executorService1.shutdown();
 
     }
 
@@ -55,3 +79,17 @@ public class ThreadPoolExamples {
         }
     }
 }
+
+/*
+O/P
+
+120
+720
+5040
+1278945280
+
+120
+5040
+1278945280
+720
+ */
