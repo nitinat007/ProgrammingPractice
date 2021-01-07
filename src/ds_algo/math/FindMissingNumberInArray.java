787f4cd72576414b6859ca83c72dd28c6d27baa0
Nitin Kumar <nitinkumar@Nitins-MacBook-Pro.local>
2021-01-07 09:54:53 +0530

    Added src/ds_algo/math/FindMissingNumberInArray.java

---
+++ src/ds_algo/math/FindMissingNumberInArray.java
@@ -0,0 +1,31 @@
+package ds_algo.math;
+
+/**
+ * Author: nitinkumar
+ * Created Date: 07/01/21
+ * Info: Array of size N contains number from 0 to N. Find the missing number in the array
+ **/
+
+public class FindMissingNumberInArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 4, 2, 7, 8, 9, 0};
+        System.out.println(missingNumberInArray(arr));
+    }
+
+    private static int missingNumberInArray(int[] arr) {
+        int sum = 0;
+        for (int i = 0; i <= arr.length; i++) {
+            sum = sum + i;
+        }
+        //instead use sum = (n)*(n+1)/2
+        int sumOfArr = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sumOfArr = sumOfArr + arr[i];
+        }
+        return sum - sumOfArr;
+    }
+}
+/*
+O/P:
+6
+ */
